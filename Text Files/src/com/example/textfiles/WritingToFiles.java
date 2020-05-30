08429fd7d0ec170c2370ce67040e523107547038
quangle36 <lewis_quang@yahoo.com.vn>
2020-05-30 22:08:44 +0700

    Reading and Writing Files

---
+++ Text Files/src/com/example/textfiles/WritingToFiles.java
@@ -0,0 +1,18 @@
+package com.example.textfiles;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class WritingToFiles {
+    public static void main(String[] args) throws IOException {
+        //create the file and open the file
+        //if the file exist it will replace the file
+        PrintWriter outputFile = new PrintWriter("OutputFile.txt");
+
+        outputFile.println("This is line 1");
+        outputFile.println("This is line 2");
+        outputFile.println("This is line 3");
+        outputFile.println("This is line 4");
+        outputFile.close();
+    }
+}
